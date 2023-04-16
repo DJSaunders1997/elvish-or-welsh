f8330cff431de13afafd46e6107572048c70201e
Dave <DJSaunders1997@gmail.com>
2023-04-16 11:46:09 +0100

    Create next question button

--- src/app.js
+++ src/app.js
@@ -17,6 +17,6 @@ let sentenceManager = new SentenceManager(json_string);
 let game = new Game(sentenceManager, ui);
 
 // Make function available in the global scope
-// Used by the Elvish and Welsh buttons in index.html
-// Should this go into ui or game class?
+// Used by the Elvish, Welsh and Next Question buttons in index.html
 window.checkAnswer = (answer) => game.checkAnswer(answer);
+window.showNextQuestion = () => game.showNextQuestion();
